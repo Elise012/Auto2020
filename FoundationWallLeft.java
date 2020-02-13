bc80ede8dcc2d2850f8c104014eb118b89988664
Elise <sarcasmisus@gmail.com>
2020-02-13 17:49:27 -0600

    Add files via upload

--- FoundationWallLeft.java
+++ FoundationWallLeft.java
@@ -8,9 +8,12 @@ public class FoundationWallLeft extends OpMode {
     ImportantStuff robot = new ImportantStuff();
 
     private enum State {
+        STATE_RIGHT,
         STATE_BACK,
         STATE_PAUSE,
         STATE_LATCH,
+        STATE_BACKUP,
+        STATE_STRAFELEFT,
         STATE_TURN,
         STATE_PAUSE2,
         STATE_RELEASE,
@@ -32,7 +35,7 @@ public class FoundationWallLeft extends OpMode {
     public void start() {
         super.start();
         Runtime.reset();
-        newState(State.STATE_BACK);
+        newState(State.STATE_RIGHT);
     }
 
     @Override
@@ -40,18 +43,27 @@ public class FoundationWallLeft extends OpMode {
         telemetry.addData("state", CurrentState);
 
         switch (CurrentState) {
-            case STATE_BACK:
+            case STATE_RIGHT:
                 if (Runtime.milliseconds() > 500) {
-                    robot.robotStuff.FLeft.setPower(-.3);
+                    robot.robotStuff.FLeft.setPower(.3);
                     robot.robotStuff.FRight.setPower(-.3);
                     robot.robotStuff.BLeft.setPower(-.3);
-                    robot.robotStuff.BRight.setPower(-.3);
+                    robot.robotStuff.BRight.setPower(.3);
+                    newState(State.STATE_BACK);
+                }
+                break;
+            case STATE_BACK:
+                if (Runtime.milliseconds() > 1175) {
+                    robot.robotStuff.FLeft.setPower(-.25);
+                    robot.robotStuff.FRight.setPower(-.25);
+                    robot.robotStuff.BLeft.setPower(-.25);
+                    robot.robotStuff.BRight.setPower(-.25);
                     newState(State.STATE_PAUSE);
                 }
                 break;
 
             case STATE_PAUSE:
-                if (Runtime.milliseconds() > 1000) {
+                if (Runtime.milliseconds() > 1300) {
                     robot.robotStuff.FLeft.setPower(0);
                     robot.robotStuff.FRight.setPower(0);
                     robot.robotStuff.BLeft.setPower(0);
@@ -66,12 +78,21 @@ public class FoundationWallLeft extends OpMode {
                     robot.robotStuff.FRight.setPower(0);
                     robot.robotStuff.BLeft.setPower(0);
                     robot.robotStuff.BRight.setPower(0);
-                    robot.robotStuff.rightHook.setPosition(.3);
-                    robot.robotStuff.leftHook.setPosition(.67);
-                    newState(State.STATE_TURN);
+                    robot.robotStuff.rightHook.setPosition(robot.robotStuff.SERVO_LATCH_DOWN);
+                    robot.robotStuff.leftHook.setPosition(robot.robotStuff.SERVO_LATCH_UP);
+                    newState(State.STATE_BACKUP);
                 }
                 break;
 
+            case STATE_BACKUP:
+                if (Runtime.milliseconds() > 1000) {
+                    robot.robotStuff.FLeft.setPower(.25);
+                    robot.robotStuff.FRight.setPower(.25);
+                    robot.robotStuff.BLeft.setPower(.25);
+                    robot.robotStuff.BRight.setPower(.25);
+                    newState(State.STATE_PAUSE2);
+                }
+                break;
             case STATE_TURN:
                 if (Runtime.milliseconds() > 1000) {
                     robot.robotStuff.FLeft.setPower(-1);
@@ -83,7 +104,7 @@ public class FoundationWallLeft extends OpMode {
                 break;
 
             case STATE_PAUSE2:
-                if (Runtime.milliseconds() > 100) {
+                if (Runtime.milliseconds() > 7750) {
                     robot.robotStuff.FLeft.setPower(0);
                     robot.robotStuff.FRight.setPower(0);
                     robot.robotStuff.BLeft.setPower(0);
@@ -98,9 +119,9 @@ public class FoundationWallLeft extends OpMode {
                     robot.robotStuff.FRight.setPower(0);
                     robot.robotStuff.BLeft.setPower(0);
                     robot.robotStuff.BRight.setPower(0);
-                    robot.robotStuff.rightHook.setPosition(.8);
-                    robot.robotStuff.leftHook.setPosition(.1);
-                    newState(State.STATE_HALT);
+                    robot.robotStuff.rightHook.setPosition(robot.robotStuff.SERVO_LATCH_UP);
+                    robot.robotStuff.leftHook.setPosition(robot.robotStuff.SERVO_LATCH_DOWN);
+                    newState(State.STATE_WALL);
                 }
                 break;
 
@@ -115,7 +136,7 @@ public class FoundationWallLeft extends OpMode {
                 break;
 
             case STATE_WALL:
-                if (Runtime.milliseconds() > 50) {
+                if (Runtime.milliseconds() > 200) {
                     robot.robotStuff.FLeft.setPower(-.5);
                     robot.robotStuff.FRight.setPower(.5);
                     robot.robotStuff.BLeft.setPower(.5);
@@ -125,7 +146,7 @@ public class FoundationWallLeft extends OpMode {
                 break;
 
             case STATE_HALT:
-                if (Runtime.milliseconds() > 200) {
+                if (Runtime.milliseconds() > 1500) {
                     robot.robotStuff.stop();
                 }
                 break;
